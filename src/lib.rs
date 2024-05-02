7435f18d2f4d02929cabb92c467fcb1b2d7247a6
FBDev64 <elzadam11@gmail.com>
2024-05-02 19:55:42 +0000

    Code

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+pub fn fh_kelvin(fahreneit: f64) -> f64 {
+	let kelvin = (fahreneit − 32) × 5 ⁄ 9 + 273.15;
+	kelvin
+}
+
+pub fn gallon_liter(gallon: f64) -> f64 {
+	let liter = gallon / 3.785412;
+	liter
+}
+
+pub fn feet_m(feet: f64) -> f64 {
+	let m = feet / 3.28084;
+	m
+}
+
+pub fn mile_km(mile: f64) -> f64 {
+	let km = mile * 1.609;
+	km
+}
+
+pub fn nmile_km(nautical: f64) -> f64 {
+	let km = nautical * 1.852;
+	km
+}
+
+pub fn pascal_bar(pascal: f64) -> f64 {
+	let bar = pascal / 100000;
+	bar
+}
+
+pub fn liter_cubic(liter: f64) -> f64 {
+	let cubic = liter / 1000;
+	cubic
+}
