c5e9bed894903b7f4ae6033ded69f1678d8e1afb
FBDev64 <elzadam11@gmail.com>
2024-05-06 19:05:57 +0000

    Rewrote 3

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-pub fn cm_m(unit1) {
-	let ans = unit1 / 100;
+pub fn cm_m(unit1: f64) {
+	let ans = unit1 / 100.0;
 	println!("{}", ans);
 } 
 
-pub fn mm_cm(unit1) {
-	let ans = unit1 / 10;
+pub fn mm_cm(unit1: f64) {
+	let ans = unit1 / 10.0;
 	println!("{}", ans)
 } 
 
-pub fn m_cm(unit1) {
-	let ans = unit1 * 100;
+pub fn m_cm(unit1: f64) {
+	let ans = unit1 * 100.0;
 	println!("{}", ans);
 }
 
